6610431020767963880a5a0945796891461f173a
Nithin-4ScreenMedia <nithin@4screenmedia.com>
2023-02-23 21:53:45 +0530

    # test 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to testing with distribution and invalidation.
+          Edit <code>src/App.js</code> and save to testing with added policies.
         </p>
         <a
           className="App-link"
