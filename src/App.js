c4e47737b940eaa555cd4357745297d57edef211
Nithin-4ScreenMedia <nithin@4screenmedia.com>
2023-02-24 17:01:11 +0530

    # build test 8

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to testing with added policies.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      Sample project to test CircleCI workflow and deploy to S3
+      </div>
+      <a className="link" href='https://circleci.com/docs/'>CircleCI Docs</a>
     </div>
+    
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sample project to test CircleCI workflow and deploy to S3/i);
   expect(linkElement).toBeInTheDocument();
 });
